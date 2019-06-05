4a5b55efa753652570ba723458b869acb54fa3c7
kosssaa <kosssaa@o2.pl>
2019-06-05 14:02:45 +0200

    Druga wersjk

--- src/main/java/pl/sda/javagda25/MainGIT.java
+++ src/main/java/pl/sda/javagda25/MainGIT.java
@@ -1,4 +1,12 @@
 package pl.sda.javagda25;
 
 public class MainGIT {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println(" Ale się tutaj zmienilo od ostatniego razu dziubaski, nie ?");
+    }
+
 }
